1069e8d58fa8703de94ed12a42d8fd79ed99a95f
Alex <alessandro.piccione.75@gmail.com>
2021-10-30 15:53:25 +0100

    use Link component

--- components/layouts.tsx
+++ components/layouts.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import Link from "next/link"
 
 type DefaultPageProps = {
     title?:string | undefined,
@@ -8,7 +9,9 @@ type DefaultPageProps = {
 export function DefaultPage(props:DefaultPageProps) {
     const {title} = props
     return <div className="default-page">
-        <div className="link-to-home"><a href="/">&larr; home</a></div>
+        <div className="link-to-home">
+            <Link href="/"><a>&larr; home</a></Link>
+        </div>
         {title && <h1 className="title">{title}</h1>}
         <div className="content">
             {props.children}
