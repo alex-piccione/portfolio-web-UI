815710d2cdc162381b884bff738daf3606131dee
Alex <alessandro.piccione.75@gmail.com>
2021-11-06 19:08:10 +0000

    cleanup

--- providers/companyProvider.tsx
+++ providers/companyProvider.tsx
@@ -14,12 +14,11 @@ const headers = {Host: host}
 // is returned as HTML and not properly managed.
 // Browser's console shows a HTML that is not readable and hides the problem.
 // UI shows an error because it cannot parse the returned HTML as JSON.
-var apiGatewayClient = createGatewayClient(signerConfig);
 
 const CompanyProvider = {
   
   getCompanies: async () =>  {
-    return axios.get(`${baseUrl}/company/all`, {headers:headers})
+    return axios.get(`${baseUrl}/company`, {headers:headers})
     .then(result =>  parser.parseCompanies(result.data))
     .catch(error => {throw Error(`Failed to call API Gateway. ${error}`)});     
   },
@@ -31,7 +30,7 @@ const parser = {
   parseCompanies: (data) => {
     try{
       const companies = (data as Array<any>).map(item => {
-        const types = item.Types.map(t => t) 
+        const types = item.Types.map((t: any) => t) 
         return {Id: item.Id, Name: item.Name, Types: types} as Company
       })
       return companies;
