c9787197002a92874acb6dd5f346db36146cafbd
Alex <alessandro.piccione.75@gmail.com>
2021-11-07 18:37:33 +0000

    2 steps rename

--- providers/BalanceProvider.ts
+++ providers/BalanceProvider.ts
@@ -1,31 +1,16 @@
 import { Balance, Company } from "../components/entities"
 import BaseProvider from "./BaseProvider"
-import CompanyProvider from "./companyProvider"
+import CompanyProvider from "./rename_CompanyProvider";
 
 class BalanceProvider extends BaseProvider {  
   async getBalance(baseCurrency:string) {
     const companies = await CompanyProvider.getCompanies();
-    return super.get(`balance?base-currency=${baseCurrency}`, (data) => parser.parseBalance_(data, companies)) 
-    //return super.get(`balance?base-currency=${baseCurrency}`, parser.parseBalance) 
+    return super.get(`balance?base-currency=${baseCurrency}`, (data) => parser.parseBalance(data, companies)) 
   }
 }
 
 const parser = {
-  parseBalance: (data:any):Balance => {
-    try {      
-      const date = new Date(data.Date)             
-      const funds = (data.FundsByCurrency as Array<any>).map(fund => {
-        return { currencyCode: fund.CurrencyCode, amount:fund.Quantity, companies: [] }
-      })
-      return {date:date, fundsByCurrency:funds}
-    }
-    catch (error) {
-      // todo: log with Sentry or similar
-      throw Error(`Failed to parse Balance. ${error}`)
-    }
-  },
-
-  parseBalance_: (data:any, companies:Company[]):Balance => {
+  parseBalance: (data:any, companies:Company[]):Balance => {
     try {      
       const date = new Date(data.Date)
       const getCompanies = (ids:string[]) => ids.map(id => { 
