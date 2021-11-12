ceba647ad25c7beccbf0d321f56817ce0d8f2495
Alex <alessandro.piccione.75@gmail.com>
2021-11-12 17:01:05 +0000

    use querySelector to find the element

--- cypress/integration/0-Navigation/visit Currencies page.spec.ts
+++ cypress/integration/0-Navigation/visit Currencies page.spec.ts
@@ -1,10 +1,11 @@
 export default describe("When click the 'Currencies' card", () => {
-  it("Should open the docs page", () => {
+  it("Should open the 'Currencies' page", () => {
     // Assert
     cy.visit("http://localhost:3000")
 
     // Act
-    cy.contains("Currencies").click()
+    //cy.get("a > h2").contains("Currencies").click()
+    cy.get("[class*='_card_']").contains("Currencies").click()
 
     // Assert
     cy.url()
