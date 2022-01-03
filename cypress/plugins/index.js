1fd738e0cfb2dd577c5f965d2c7729b399f1c437
Alex <alessandro.piccione.75@gmail.com>
2022-01-03 11:24:05 +0000

    correct pugind/index.js syntax

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -9,10 +9,9 @@
 // https://on.cypress.io/plugins-guide
 // ***********************************************************
 
-
-const browserify = require('@cypress/browserify-preprocessor')
-const resolve = require("resolve")
-const cucumber = require("cypress-cucumber-preprocessor").default
+const browserify = require('@cypress/browserify-preprocessor');
+const cucumber = require('cypress-cucumber-preprocessor').default;
+const resolve = require('resolve');
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
@@ -22,13 +21,12 @@ const cucumber = require("cypress-cucumber-preprocessor").default
  */
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
-  const options = {
-    ...browserify.defaultOptions,
-    typeScript: resolve.sync("typescript", { basedir: config.projectRoot }),
-  }
-
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-  on("file:preprocessor", cucumber(options))
 
+  const options = {
+    ...browserify.defaultOptions,
+    typescript: resolve.sync('typescript', { baseDir: config.projectRoot }),
+  }
+  on('file:preprocessor', cucumber(options))
 }
