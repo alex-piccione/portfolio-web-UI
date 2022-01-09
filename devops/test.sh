36a4f793e4811a232aa81cfa5aed16b530678b1e
Alex <alessandro.piccione.75@gmail.com>
2022-01-09 21:55:53 +0000

    deploy

--- devops/test.sh
+++ devops/test.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-mkdir ./test
+# https://www.computerhope.com/unix/uset.htm
+# Mark variables that are modified or created for "export";
+#    environment variables set in this way will be passed on to the environments of any subsequent commands.
+# is thia needed?
+#set -a
+
+echo var1 = $1
+
+envsubst '${AWS_API_ID} $1' < ../.env > ../.env_new
