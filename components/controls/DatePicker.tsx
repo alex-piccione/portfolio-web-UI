8d65294817838c08e002f1c39fba0745df878b61
Alex <alessandro.piccione.75@gmail.com>
2022-01-23 15:02:10 +0000

    cleanup

--- components/controls/DatePicker.tsx
+++ components/controls/DatePicker.tsx
@@ -8,30 +8,22 @@ export default function _(props:{initialDate?:Date, className?:string, onChange:
   const [date, setDate] = useState(props.initialDate||new Date());
   const router = useRouter()    
   const locale = getUserLocale(router.locale)
-  //console.log("router.locale: " + router.locale)
 
   let dateFormat = locale.formatLong.date({ width: "short" })
   console.log("dateFormat: " + dateFormat)
-  //var calendar:DatePicker = undefined
 
   const handleChange = (date) => {
     setDate(date) 
     props.onChange(date)
   }
 
-  // <!--<input type="text" className={props.className || "form-control"} value={date.toLocaleDateString()} onClick={_ => calendar.setOpen(true)} /> -->
-  
-  //ref={r => calendar = r} 
-  return <>
-    
-    <DatePicker
-      locale={locale}
-      dateFormat={dateFormat}
-      selected={date}
-      onChange={handleChange}
-      wrapperClassName="date-picker-wrapper"  
-      calendarClassName="calendar"  
-      className={props.className || "form-control"}       
-    />
-  </>
+  return <DatePicker
+    locale={locale}
+    dateFormat={dateFormat}
+    selected={date}
+    onChange={handleChange}
+    wrapperClassName="date-picker-wrapper"  
+    calendarClassName="calendar"  
+    className={props.className || "form-control"}       
+  />
 }
