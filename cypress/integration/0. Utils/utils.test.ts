ec132796ca2b475b7c636f700799f8cc44af15f4
Alex <alessandro.piccione.75@gmail.com>
2022-01-30 21:00:53 +0000

    correct test

--- cypress/integration/0. Utils/utils.test.ts
+++ cypress/integration/0. Utils/utils.test.ts
@@ -1,22 +1,14 @@
-
-
-
-// en-GB,en;q=0.9
-
-import { parseAcceptLanguageHeader } from "../../../common/utils"
-
-parseAcceptLanguageHeader
+import { getLocale } from "../../../common/utils"
 
 const test =
-  describe(parseAcceptLanguageHeader.name, () => {
+  describe(getLocale.name, () => {
 
-    //const testCases = []
     it("detect the right locale", () => {
       const testCases = [
           ["en-GB,en;q=0.9", "en-GB"],
           ["zh-CN;q=0.6, zh;q=0.4", "zh-CN"],
         ].map( 
-        ([input, expectedResult]) => expect(parseAcceptLanguageHeader(input)[0]).equals(expectedResult) 
+        ([input, expectedResult]) => expect(getLocale(input)).equals(expectedResult) 
     )})
   })
 
