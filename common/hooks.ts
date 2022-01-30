a1267e678f2cb2b81200806a26e3916ec49cab46
Alessandro <876490+alex-piccione@users.noreply.github.com>
2022-01-30 21:03:54 +0000

    Feature/36 detect locale (#38)
    * create useLocale hook

--- common/hooks.ts
+++ common/hooks.ts
@@ -1,3 +1,35 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react"
+import { getLocale } from "./utils"
 
-export const useMountEffect = handler => useEffect(handler, []);
+const defaultLocale = "it"
+
+/**
+ * useMountEffect
+ * hook to run a function when component mount
+ */
+export const useMountEffect = handler => useEffect(handler, [])
+
+/**
+ * useLocale
+ * hook to get the locale
+ * @method locale: get the locale from browser. Default is "it".
+ */
+ export const useLocale = () => {
+
+  const [locale, setLocale] = useState(defaultLocale)
+  const [language, setLanguage] = useState("it") 
+  useEffect(() => {
+    console.log("useLocale::window.navigator.language", window.navigator.language)
+    const locale = getLocale(window.navigator.language) || defaultLocale
+    const language = locale.substring(0, 2)
+    setLocale(locale)
+    setLanguage(language)
+  }, [])
+  
+  return {
+    locale: locale,
+    language: language,
+    setLocale,
+    setLanguage,
+  }
+}
--- components/Spinner.tsx
+++ components/Spinner.tsx
@@ -1,7 +1,5 @@
-
 const Spinner = (props:{id?:string}) =>
   <div id={props.id} className="spinner-border text-secondary" role="status">
-    <span className="visually-hidden">Loading...</span>
   </div>
 
 export default Spinner 
