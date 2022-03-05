18829ecb16493977300b8035abfd22b219cb95d3
Alex <alessandro.piccione.75@gmail.com>
2022-03-05 09:55:06 +0000

    Create Dockerfile 400MB image

--- devops/create-docker-image.sh
+++ devops/create-docker-image.sh
@@ -3,6 +3,7 @@
 # ".." is the path for the context
 
 docker image build -t portfolio-app:2 -f Dockerfile ..
+docker image build -t portfolio-app:5.7 -f devops/Dockerfile .
 echo "Docker image created"
 
 echo "Docker images:"
@@ -11,7 +12,14 @@ docker image ls
 
 ### DOCS for run: https://docs.docker.com/engine/reference/commandline/run/
 
-docker container run -i -p 8080:3000 portfolio-app:3
+docker container run -p 8080:3000 portfolio-web:0.4
+docker container run -p 8080:3000 --name portfolio-app portfolio-app:5 
 docker container run -it --entrypoint sh portfolio-app:1
+
+docker container run -p 8080:3000 --name portfolio-app --env a_AWS_REGION=aaa portfolio-app:5 
+docker container run -p 8080:3000 --name portfolio-app --env-file .env.local portfolio-app:5 
+
+docker container run -p 8080:3000 --name portfolio-app-5.4 --env-file .env.local portfolio-app:5.4
+
 
 read -p "Press [Enter] key to continue..."
