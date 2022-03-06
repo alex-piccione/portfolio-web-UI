bd126cb8c5c79f49c9710ce715c392011cb75b93
Alex <alessandro.piccione.75@gmail.com>
2022-03-06 10:22:42 +0000

    add a Docker.2 file example for standalone NextJs build

--- devops/create-docker-image.sh
+++ devops/create-docker-image.sh
@@ -3,7 +3,8 @@
 # ".." is the path for the context
 
 docker image build -t portfolio-app:2 -f Dockerfile ..
-docker image build -t portfolio-app:0.12 -f devops/Dockerfile .
+docker image build -t portfolio-app:0.18 -f devops/Dockerfile .
+docker image build -t portfolio-app:2.18 -f devops/Dockerfile.2 .
 echo "Docker image created"
 
 echo "Docker images:"
@@ -16,10 +17,9 @@ docker container run -p 8080:3000 portfolio-web:0.4
 docker container run -p 8080:3000 --name portfolio-app portfolio-app:5 
 docker container run -it --entrypoint sh portfolio-app:1
 
-docker container run -p 8080:3000 --name portfolio-app --env a_AWS_REGION=aaa portfolio-app:5 
-docker container run -p 8080:3000 --name portfolio-app --env-file .env.local portfolio-app:5 
-
-docker container run -p 8080:3000 --name portfolio-app-0.12 --env-file .env.local portfolio-app:0.12
+docker container run -p 8080:3000 --name portfolio-app-0.18 --env-file .env.local portfolio-app:0.18
+docker container run -p 8080:3000 --name portfolio-app-2.18 --env-file .env.local portfolio-app:2.18
 
 
 read -p "Press [Enter] key to continue..."
+
